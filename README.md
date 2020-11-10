ea8c05dd5b9158d9eae7ad222ed067629c30b8bb
Surekha-16 <surekhag02@gmail.com>
2020-11-11 01:48:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-fc314f
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-fc314f)
---
+++ index.js
@@ -0,0 +1,25 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+
+ /* const table = document.getElementById('myTable');
+  const row = table.insertRow(2);
+  const cell1 = row.insertCell(0);
+  const cell2 = row.insertCell(1);
+  cell1.innerHTML = 'AAA-1812';
+  cell2.innerHTML = '$2,010,926.10' + ' <br><span style="color: green;">+0.21%/$38,881.63</span>';
+   const row1 = table.insertRow(3);
+  const cell3 = row1.insertCell(0);
+  const cell4 = row1.insertCell(1);
+  cell3.innerHTML = 'AAA-3810';
+  cell4.innerHTML = '$10,050,054.07' + ' <br><span style="color: green;">+0.07%/$8,916.69</span>';
+  const row2 = table.insertRow(4);
+  const cell5 = row2.insertCell(0);
+  row2.colspan = 2;
+  cell5.innerHTML = 'Load More';*/
+  
+
+  
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
